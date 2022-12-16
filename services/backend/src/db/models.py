7ce44060759de0991bde9fb08d446e4a91cb6685
Alexandra Ryabinina <aoryabinina@gmail.com>
2022-12-16 20:31:43 +0300

    add sqlalchemy

---
+++ services/backend/src/db/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, String
+from sqlalchemy.dialects.postgresql import UUID
+
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column("id", UUID(as_uuid=True), primary_key=True, index=True)
+    username = Column("username", String(128), nullable=False, unique=True)
+    password = Column("password", String(128), nullable=False)
--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -1,15 +1,27 @@
-from typing import Union
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
 
-from fastapi import FastAPI
+from .db import models
+from .db.database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-@app.get("/")
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get('/')
 def read_root():
-    return {"Hello": "World"}
+    return {'Hello': 'World'}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get('/users')
+def read_users(db: Session = Depends(get_db)):
+    return db.query(models.User).all()
