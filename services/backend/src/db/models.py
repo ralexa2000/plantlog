a5484e65afc975004671b26d64a6679f2d27937d
Alexandra Ryabinina <aoryabinina@gmail.com>
2022-12-16 20:32:45 +0300

    fix style

--- services/backend/src/db/models.py
+++ services/backend/src/db/models.py
@@ -4,8 +4,8 @@ from sqlalchemy.dialects.postgresql import UUID
 from .database import Base
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = 'users'
 
-    id = Column("id", UUID(as_uuid=True), primary_key=True, index=True)
-    username = Column("username", String(128), nullable=False, unique=True)
-    password = Column("password", String(128), nullable=False)
+    id = Column('id', UUID(as_uuid=True), primary_key=True, index=True)
+    username = Column('username', String(128), nullable=False, unique=True)
+    password = Column('password', String(128), nullable=False)
