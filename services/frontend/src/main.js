52c5cb2c68f5ee1a6abf22edb78b68241733e101
Alexandra Ryabinina <aoryabinina@gmail.com>
2023-03-02 13:44:05 +0300

    add frontend to docker

--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
+import 'bootstrap/dist/css/bootstrap.css';
+import { createApp } from 'vue';
+import axios from 'axios';
+
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:8000/';  // the FastAPI backend
+
+app.use(router);
+app.mount("#app");
